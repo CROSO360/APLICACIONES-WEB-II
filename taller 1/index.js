01d6b173eaf89efb1a54a88fea15d65ce30c3d1c
Cristian Bonilla Moreira <80973867+CROSO360@users.noreply.github.com>
2023-04-06 21:54:42 -0500

    Add files via upload

---
+++ taller 1/index.js
@@ -0,0 +1,5 @@
+//mediante require se importan la funcion y las entidades
+const { MostrarInfo, tutor, tutoria, tutorado } = require('./entidades');
+
+//se llama a la funcion con sus respectivos parametros
+MostrarInfo(tutor, tutorado, tutoria);
